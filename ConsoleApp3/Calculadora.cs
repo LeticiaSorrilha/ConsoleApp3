059a6c966032683011f9ebaf5bd85c31488d6cee
Leticia Sorrilha Souza <leticiasorrilhageo@gmail.com>
2020-12-13 22:02:29 -0300

    Desenvolvimento da Calculadora com sobrecarga de Função.

--- ConsoleApp3/Calculadora.cs
+++ ConsoleApp3/Calculadora.cs
@@ -1,8 +1,9 @@
-﻿namespace Projeto
+﻿using System;
+
+namespace Projeto
 {
     public class Calculadora
     {
-        // Soma(double n1, double n2)
         public double Soma(double n1, double n2)
         {
             return n1 + n2;
@@ -10,11 +11,66 @@
         public double Soma(params double[] numeros)
         {
             double resultado = 0.0;
-            foreach (double numero in numeros)
+            foreach (double numero in numeros)//para cada numero no array de numeros faça:
             {
-                resultado += numero;
+                resultado += numero;//soma resultado + numero e atribui o resultado da soma na variavel resultado
             }
             return resultado;
         }
-    }
+
+        public double Subtrai(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Subtrai(params double[] numeros)
+        {
+            double resultado = 0.0;
+            foreach (double numero in numeros)
+            {
+                resultado -= numero;
+            }
+            return resultado;
+        }
+
+        public double Multiplica(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Multiplica(params double[] numeros)
+        {
+            double resultado = 1.0;
+            foreach (double numero in numeros)
+            {
+                resultado *= numero;
+            }
+            return resultado;
+        }
+        
+        public double Divide(double n1, double n2)
+        {
+            double resultado = 0.0;
+            resultado = n1 / n2;
+            if (double.IsNaN(resultado))//divisão por zero em tipo double não gera exception, então
+            {                           //se a variavel resultado for um NaN (Not a Number) força a geração de uma exception
+                throw new DivideByZeroException("Erro de divisão por 0.");
+            }
+            return resultado;
+        }
+
+        public double Divide(params double[] numeros)
+        {                                   //a divisão precisava começar com um número que não fosse 0, então peguei o número 
+            double resultado = numeros[0];  // da primeira posição do array de numeros que foi passado de parametro
+            for(int i =1; i<numeros.Length; i++) //percorre o array a partir da segunda posição e faz as divisões
+            {
+                resultado /= numeros[i];
+            }
+            if (double.IsNaN(resultado))
+            {
+                throw new DivideByZeroException("Erro de divisão por 0.");
+            }
+            return resultado;
+        }
+    }   
 }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,13 +2,12 @@
 using System.Globalization;
 
 namespace Projeto
-{  
+{
     class Program
-    { 
+    {
         public static void Main(String[] args)
         {
-            Calculadora calc;
-            calc = new Calculadora();
+            Calculadora calc = new Calculadora();
 
             double[] numeros = { 1.2, 1.3, 1.4, 2.4, 4.4 };
 
@@ -18,11 +17,44 @@ namespace Projeto
             double resultado = calc.Soma(n1, n2);
             Console.WriteLine("resultado 1 = " + resultado);
 
-
-            double resultado2 = calc.Soma(numeros);
+            double resultado2 = calc.Soma(numeros);//soma array de números e imprime resultado
             Console.WriteLine("resultado 2 =" + resultado2);
 
 
+            double resultado3 = calc.Subtrai(n1, n2);
+            Console.WriteLine("resultado 3 = " + resultado3);
+
+            double resultado4 = calc.Subtrai(numeros);
+            Console.WriteLine("resultado 4 =" + resultado4);
+
+            double resultado5 = calc.Multiplica(n1, n2);
+            Console.WriteLine("resultado 5 = " + resultado5);
+
+            double resultado6 = calc.Multiplica(numeros);
+            Console.WriteLine("resultado 6 =" + resultado6);
+
+            try
+            {//captura a exception gerada pela função Divide da classe Calculadora.
+                double resultado7 = calc.Divide(n1, n2);
+                Console.WriteLine("resultado 7 = " + resultado7);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                double resultado8 = calc.Divide(numeros);
+                Console.WriteLine("resultado 8 =" + resultado8);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            /*os blocos try-catch foram feitos separados para o caso de ocorrer uma exception
+             no calculo da variavel resultado7, esta não interfira no cálculo da variável resultado8
+             */
             Console.Read();
         }
     }
